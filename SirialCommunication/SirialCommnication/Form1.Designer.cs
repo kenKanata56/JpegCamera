155d4a7b21331ca9395dc30bee3ac92cdb8cabb1
kenta <tana.kenta09@gmail.com>
2016-05-29 21:54:30 +0900

    first commit

---
+++ SirialCommunication/SirialCommnication/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SirialCommnication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(29, 24);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.comboBox3.Location = new System.Drawing.Point(247, 24);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(440, 24);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(116, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(393, 63);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(202, 254);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Received Here";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(7, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(189, 219);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(393, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Open Port";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(503, 334);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 28);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Close Port";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Port Names";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(438, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Status";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 299);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 376);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Name = "Form1";
+            this.Text = "Serial Communication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SirialCommunication/SirialCommnication/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace SirialCommnication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorts();
+        }
+
+        void getAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            comboBox2.Items.AddRange(ports);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox2.Text == "" || comboBox3.Text == "")
+                {
+                    richTextBox1.Text = "Please select port settings";
+                }
+                else
+                {
+                    serialPort1.PortName = comboBox2.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox3.Text);
+                    serialPort1.Open();
+                    progressBar1.Value = 100;
+                    button3.Enabled = false;
+                    button4.Enabled = true;
+                    serialPort1.RtsEnable = false;// Enable RTS for flow control
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                richTextBox1.Text = "Unauthorized Access";
+            }
+
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+            serialPort1.Close();
+            progressBar1.Value = 0;
+            button4.Enabled = false;
+            button3.Enabled = true;
+            
+        }
+
+
+        byte[] rec = new byte[1024]; 
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            System.Threading.Thread.Sleep(1000);
+
+            int state = 0;
+
+            for (; ; )
+            {
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                if (RecDataCheck())
+                {
+                    if (state == 0)
+                    {
+                        if (StartDataWait() == 1)
+                        {
+                            state = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (DataNumberWait() != 1)
+                        {
+                            state = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(10);
+                }
+            }
+        }
+        MemoryStream ms;
+        ArrayList rcv_data = new ArrayList();
+        int pictsize = 0;
+        private int StartDataWait()
+        {
+            byte[] start = new byte[2];
+            start[0] = 0xFF;
+            start[1] = 0xD8;
+            
+            for (int i = 0; i < 2; i++)
+            {
+
+                if (RecData(0, 1) < 0)
+                {
+                    return -1;
+                }
+                rcv_data.Add(rec[0]);
+                if (rec[0] != start[i])
+                {
+                    return -1;
+                }
+            }
+            DisposeMs();
+            ms = new MemoryStream();
+            ms.Write(start, 0, 2);
+
+            int count = rcv_data.Count;
+
+            byte data1 = (byte)rcv_data[count - 3];
+            byte data2 = (byte)rcv_data[count - 4];
+
+            pictsize = data1 * 256 + data2;
+
+
+            if (RecData(0, pictsize) < 0)
+            {
+                return -1;
+            }
+            ms.Write(rec, 0, pictsize - 2);
+            StatusText("Start");
+            return 1;
+        }
+
+        private int DataNumberWait(){
+
+            int[] counter = new int[2];
+            int cnt = 1;
+
+            for (; ; )
+            {
+                counter[1] = (cnt & 0xFF00) >> 8;
+                counter[0] = (cnt & 0x00FF);
+
+                for (int i = 0; i < 2; i++)
+                {
+
+                    if (RecData(0, 1) < 0)
+                    {
+                        return -1;
+                    }
+
+                    if (rec[0] != counter[i])
+                    {
+                        return -1;
+                    }
+                }
+
+                int size = getData();
+                if (0 > size || size > 1024)
+                {
+                    return -1;
+                }
+                if (RecData(0, size + 2) < 0)
+                {
+                    return -1;
+                }
+                ms.Write(rec, 0, size);
+                StatusText("packet[" + cnt + "] size:" + size);
+
+                if (size != pictsize)
+                {
+                    DateTime dateNow = DateTime.Now;
+                    String fileName = dateNow.ToString("yyyyMMddHHmmss");
+                    string fname = "pict" + fileName + ".jpg";
+                    FileStream fs = new FileStream(fname,FileMode.Create,FileAccess.Write);
+                    int fileSize = (int)ms.Length;
+                    byte[] buf = new byte[1024];
+                    int remain = fileSize;
+                    int readSize;
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    while (remain > 0)
+                    {
+                        readSize = ms.Read(buf, 0, Math.Min(1024, remain));
+                        fs.Write(buf, 0, readSize);
+                        remain -= readSize;
+                    }
+                    fs.Dispose();
+                    StatusText("SUCCESS!!");
+                    StatusText("Save;"+fname);
+
+                    this.Invoke((MethodInvoker)(() => {
+
+//pictureBox1.Image.Dispose();
+                            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                            pictureBox1.Image = System.Drawing.Image.FromFile(@fname);
+
+                        
+                    }));
+                    return -1;
+                }
+                cnt++;
+            }
+        }
+
+        private void StatusText(String data)
+        {
+            this.Invoke((MethodInvoker)(() => { richTextBox1.Focus(); }));
+            this.Invoke((MethodInvoker)(() => { richTextBox1.AppendText(data + Environment.NewLine); }));
+
+        }
+
+        private void DisposeMs()
+        {
+            try
+            {
+                ms.Dispose();
+            }
+            catch
+            {
+            }
+        }
+        
+
+        private int getData(){
+            int datasize = 0;
+            if(RecData(0,2) < 0){
+                return -1;
+            }
+            datasize = rec[0] + rec[1] * 256;
+            return datasize;
+        }
+
+
+
+
+        private int RecData(int offset, int size)
+        {
+            int i;
+
+            if (serialPort1.IsOpen == false)
+            {
+                return -1;
+            }
+
+            try
+            {
+                i = serialPort1.Read(rec, offset, size);
+            }
+            catch (TimeoutException)
+            {
+                i = 0;
+            }
+            catch (InvalidOperationException)
+            {
+                i = -1;
+            }
+            catch(Exception){
+                i = -1;
+            }
+            return i;
+        }
+
+        private bool RecDataCheck(){
+            if(serialPort1.BytesToRead <= 0){
+                return false;
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
